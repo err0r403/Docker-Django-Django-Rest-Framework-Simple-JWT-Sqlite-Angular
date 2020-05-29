7f83e9c084e4eea0ab17e67d17fb7484bb7f7141
Thiago <tdias@cloudata.cl>
2020-05-29 01:28:03 -0400

    add Drf-yasg Swagger API Docs

--- web/settings.py
+++ web/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'corsheaders',
+    'drf_yasg',
 
 ]
 
--- web/urls.py
+++ web/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls import include, url
 from rest_framework.routers import DefaultRouter
 from tickets.api import TicketViewSet
 from rest_framework_simplejwt.views import (
@@ -9,6 +10,24 @@ from rest_framework_simplejwt.views import (
 )
 
 from django.views.generic.base import TemplateView
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Tickets API",
+      default_version='v1',
+      description="Django Rest Framework API Tickets",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="juandoe@tickets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 router = DefaultRouter()
 router.register(r'tickets', TicketViewSet, 'api-ticket')
@@ -22,4 +41,7 @@ urlpatterns = [
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api/', include(router.urls)),
     path('', TemplateView.as_view(template_name='index.html'), name="home"),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
