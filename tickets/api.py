7da50a853a1aa944464e6a6250e3c5ec7c046784
Thiago <tdias@cloudata.cl>
2020-05-29 17:38:20 -0400

    add my-account url + myuserapiview

--- tickets/api.py
+++ tickets/api.py
@@ -1,9 +1,13 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, permissions, status
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.contrib.auth.models import User
 
 from .models import Ticket
-from .serializers import TicketSerializer
+from .serializers import TicketSerializer, UserSerializer
+
+from rest_framework.authentication import TokenAuthentication
 
 class TicketViewSet(viewsets.ModelViewSet):
     model = Ticket
@@ -24,3 +28,13 @@ class TicketViewSet(viewsets.ModelViewSet):
         if self.request.query_params.get("my-tickets"):
             return Ticket.objects.filter(author=self.request.user).order_by('created_at')
         return Ticket.objects.all().order_by('created_at')
+
+class MyUser(APIView):
+
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        permissions = [permission for permission in request.user.get_all_permissions()]
+        return Response({
+            "permissions": permissions
+        })
--- web/urls.py
+++ web/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls import include, url
 from rest_framework.routers import DefaultRouter
-from tickets.api import TicketViewSet
+from tickets.api import TicketViewSet, MyUser
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -30,12 +30,14 @@ schema_view = get_schema_view(
 )
 
 router = DefaultRouter()
-router.register(r'tickets', TicketViewSet, 'api-ticket')
 
+router.register(r'tickets', TicketViewSet, 'api-ticket')
+# router.register(r'users', UserViewSet, 'api-user')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api/my-account/', MyUser.as_view()),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
