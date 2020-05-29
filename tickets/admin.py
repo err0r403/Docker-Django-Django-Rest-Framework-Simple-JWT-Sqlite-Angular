50f15f4e85d75612ba8f81aafab9d6b156e08dd4
Thiago <tdias@cloudata.cl>
2020-05-29 01:22:09 -0400

    add admin list_display, list_filter, search_fields

--- tickets/admin.py
+++ tickets/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from .models import Ticket
 
-admin.site.register(Ticket)
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'state')
+    list_filter = ('author', "state",)
+    search_fields=["title",]
